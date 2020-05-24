7302e11840961bfdcfde1582f841bd46509bf0cf
asdf-codes <oo@noadjective.com>
2020-05-24 12:59:25 -0400

    minor changes to the copy + new logo and some picture movements

--- src/templates/allProjects.js
+++ src/templates/allProjects.js
@@ -67,7 +67,7 @@ export const pageQuery = graphql`
                 slug
                 featureImage {
                     childImageSharp {
-                        fluid {
+                        fluid{
                             ...GatsbyImageSharpFluid
                                 }
                             }
